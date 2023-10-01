79c1197c68a82c9ed9c29d29d09e32831ccfa46d
aoto <ton2poko@gmail.com>
2023-10-01 23:53:08 +0900

    db:migrateができない

--- db/migrate/20230926080320_add_devise_to_users.rb
+++ db/migrate/20230926080320_add_devise_to_users.rb
@@ -6,6 +6,7 @@ class AddDeviseToUsers < ActiveRecord::Migration[6.1]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      
 
       ## Recoverable
       t.string   :reset_password_token
@@ -32,9 +33,11 @@ class AddDeviseToUsers < ActiveRecord::Migration[6.1]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-
+     
       # Uncomment below if timestamps were not included in your original model.
-      # t.timestamps null: false
+      t.string :username
+      t.string :profile_image_id
+      t.timestamps null: false
     end
 
     add_index :users, :email,                unique: true
